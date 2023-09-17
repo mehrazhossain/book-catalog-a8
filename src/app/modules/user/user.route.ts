aea087494adcead9c662cc8d307a9b68bcc6c34f
Mehraz Hossain <mehrazhossain54@gmail.com>
2023-09-17 21:58:18 +0600

    Add get single user by admin

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.post('/auth/signup', UserController.createUser);
 router.post('/auth/signin', UserController.loginUser);
 router.get('/users', UserController.getAllUsers);
+router.get('/users/:id', UserController.getSingleUser);
 
 export const userRoutes = router;
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -90,8 +90,29 @@ const getAllUsers = async () => {
   return users;
 };
 
+const getSingleUser = async (id: string): Promise<Partial<User> | null> => {
+  const result = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      password: false,
+      role: true,
+      contactNo: true,
+      address: true,
+      profileImg: true,
+    },
+  });
+
+  return result;
+};
+
 export const UserService = {
   createUser,
   getAllUsers,
   loginUser,
+  getSingleUser,
 };
